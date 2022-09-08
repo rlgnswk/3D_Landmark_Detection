190f10409c79c6c8534fec2b675fda60e4ea9cd5
rlgnswk <rlgnswk12@gmail.com>
2022-09-08 06:31:59 +0000

    ing data aug

--- data_load.py
+++ data_load.py
@@ -13,6 +13,11 @@ from PIL import Image
 import pandas
 import matplotlib.pyplot as plt
 
+# References
+# https://github.com/FunkyKoki/Laplace_Landmark_Localization/blob/e8067bcd90c66227676cc1482e9defc3c9baa659/datasets/datasetsTools.py#L100
+# https://tutorials.pytorch.kr/beginner/data_loading_tutorial.html
+# https://towardsdatascience.com/face-landmarks-detection-with-pytorch-4b4852f5e9c4
+
 class FaceLandMark_Loader(Dataset):
     def __init__(self, root, IsAug = True):
         super(FaceLandMark_Loader, self).__init__()
@@ -38,8 +43,6 @@ class FaceLandMark_Loader(Dataset):
         self.std = 0.2
         self.mean = 0
 
-        
-
         #using augmentation functions which do not effect on landmarks position 
     def __getitem__(self, idx):
         
@@ -54,6 +57,10 @@ class FaceLandMark_Loader(Dataset):
         bbox_leftcorner = pandas.read_csv(self.bbox_leftcorner_coord_path +'/'+self.bbox_list[idx],  header=None, sep=' ')
         bbox_leftcorner = np.asarray(bbox_leftcorner) # shape : (2, 1) # x, y
         
+        #crop image 256x256 including face info --> how? # crop이 가장 위에 가야지 계산 효율성이 좋을 듯!
+        crop_img = img.crop((bbox_leftcorner[0][0], bbox_leftcorner[0][1], bbox_leftcorner[0][0]+ 256, bbox_leftcorner[0][1] + 256)
+        crop_ladmks = self._landmark_processing4crop(ldmks, bbox_leftcorner)
+
         '''
         Paper:
         During training, we perform data augmentation including
@@ -62,20 +69,46 @@ class FaceLandMark_Loader(Dataset):
         '''
 
         #blurs, modulations to brightness and contrast, addition of noise, and conversion to grayscale. < -- does not be related with landmark
-        img = F.adjust_brightness(img, brightness_factor = random.uniform(0.5,1.5)) # brightness_factor 0(black) ~ 2(white)
-        img = F.adjust_contrast(img, contrast_factor = random.uniform(0.5,1.5)) # contrast_factor 0(solid gray) ~ 2
-        img = F.gaussian_blur(img, kernel_size = random.randint(3, 7))
-        img = F.rgb_to_grayscale(img, num_output_channels =3)
-        img = img + torch.randn(img.size()) * self.std + self.mean 
+        crop_img = F.adjust_brightness(crop_img, brightness_factor = random.uniform(0.5,1.5)) # brightness_factor 0(black) ~ 2(white)
+        crop_img = F.adjust_contrast(crop_img, contrast_factor = random.uniform(0.5,1.5)) # contrast_factor 0(solid gray) ~ 2
+        crop_img = F.gaussian_blur(crop_img, kernel_size = random.randint(3, 7))
+        crop_img = F.rgb_to_grayscale(crop_img, num_output_channels =3)
+        crop_img = crop_img + torch.randn(crop_img.size()) * self.std + self.mean 
         
-        #crop image 256x256 including face info --> how? # crop이 가장 위에 가야지 계산 효율성이 좋을 듯!
-    
+        #rotations, perspective warps, < -- landmark also should be changed 
+
+        crop_img, crop_ladmks = self._rotate(crop_img , crop_ladmks)
+        crop_img, crop_ladmks = self._perspective_warp(crop_img , crop_ladmks)
+
         #conduct augmentation --> how handle the annotation simultaneously??
 
-        return landmark_GT, input_Image, crop_info
+        return landmark_GT, crop_img, bbox_leftcorner
 
     def __len__(self):
         return len(self.img_list)
+
+    def _landmark_processing4crop(self, ldmks, bbox_leftcorner):
+        #make ldmks point corresponding to cropped image
+        ldmks = ldmks - [bbox_leftcorner[0][1] ,bbox_leftcorner[0][0]] # landmarks = landmarks - [left, top] 
+        return ldmks
+
+    def _rotate(self, crop_img, crop_ladmks, angle = 0, imgWidth =256, imgHeight =256):
+        
+        rotated_img = crop_img.rotate(angle)
+        center = np.array([imgWidth / 2.0, imgHeight / 2.0], dtype=np.float32)
+
+        c, s = np.cos(angle), np.sin(angle)
+        rot_mat = np.array(((c,-s), (s, c)))
+        #center -> 0 , rotate , zero center -> original center
+        rotated_ladmks = np.matmul(target_center - center, rot) + center
+
+        return rotated_img, rotated_ladmks
+    
+    def _perspective_warp(self, crop_img, crop_ladmks):
+
+        shearMat = np.array([[1., cX, 0.], [cY, 1., 0.]], dtype=np.float32)
+
+        pass
 
 def get_dataloader(dataroot, batch_size, IsSuffle = True):
     dataset = MotionLoader(dataroot)
